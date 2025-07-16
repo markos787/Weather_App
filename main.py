47fc9ce99df16c7d43e3fb4aaf5deaf5dc76954d
markos787 <marekweis01@gmail.com>
2025-07-16 18:21:49 +0200

    init: imports

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import requests
+import pytz
+from tkinter import *
+from geopy.geocoders import Nominatim
+from tkinter import ttk, messagebox
+from timezonefinder import TimezoneFinder
+from datetime import *
+from PIL import Image, ImageTk
+
+
