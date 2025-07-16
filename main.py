4e94be06f29be2224b2fdba03551624160153bed
markos787 <marekweis01@gmail.com>
2025-07-16 18:33:41 +0200

    wip: root init

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ from datetime import *
 from PIL import Image, ImageTk
 
 
+root=Tk()
+root.title("Weather Forecast")
+root.geometry('890x470+300+300') # 300 - distance from left and bottom of the screen
+root.configure(bg='dodgerblue')
+root.resizable(False, False)
+
+
+
+root.mainloop()
